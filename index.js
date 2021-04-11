3bff30a2e73b60d63ad1a82df86773a769f3d6fc
johnshvets <johnshvets@gmail.com>
2021-04-11 17:46:22 +0300

    add index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
+const argv = yargs(hideBin(process.argv)).argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
